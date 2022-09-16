d59674f726e5baae0f9a2897712b41329f54dd9b
Andrew Pham <andrew.pham@shopify.com>
2022-09-15 20:51:00 -0400

    Add score feature to test

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -5,11 +5,18 @@ import json
 with open('me-capitals.json', 'r') as f:
     data = json.load(f)
 
+# initialize total as the length of the cards array
+total = len(data["cards"])
+# initialize score as 0
+score = 0
+
 for item in data["cards"]:
     guess = input(item["q"] + " > ")
     
-    if guess == item["a"]:
-        print("Correct!")
+    if guess.lower() == item["a"].lower():
+        score += 1
+        print(f"Correct! Current score: {score}/{total}")
     else:
         print(f'Inccorect! The correct answer was {item["a"]}.')
+        print(f"Correct! Current score: {score}/{total}")
 
